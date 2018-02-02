e82b9e728cac7cc89adef927a8e23284f4a79248
Joshua Ciffer <Joshua.Ciffer@motsd.org>
2018-02-01 19:29:13 -0500

    Setup basic client gui.

--- Instant Messenger/src/src/instantMessenger/client/ClientGUI.java
+++ Instant Messenger/src/src/instantMessenger/client/ClientGUI.java
@@ -10,6 +10,8 @@ import src.instantMessenger.util.Constants;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.JButton;
 
 /**
@@ -26,14 +28,18 @@ public final class ClientGUI extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	private JMenu clientMenu;
-	
+
 	private JTextArea chatFeed;
-	
+
 	private JScrollPane chatFeedPane;
 
-	private JTextField messageField;
+	private JTextArea messageField;
+
+	private JScrollPane messageFieldPane;
 
 	private JButton sendButton;
+	
+	private KeyListener enterKeyListener;
 
 	ClientGUI() {
 		super("Instant Messenger Client");
@@ -41,42 +47,88 @@ public final class ClientGUI extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		setResizable(true);
-		
+		setLayout(null);
+
 		chatFeed = new JTextArea();
 		chatFeed.setEditable(true);
 		chatFeed.setLineWrap(true);
 		chatFeed.setWrapStyleWord(true);
 		chatFeed.setFont(Constants.CHAT_FONT);
-		
+
 		chatFeedPane = new JScrollPane(chatFeed);
 		chatFeedPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		chatFeedPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+		messageField = new JTextArea();
+		messageField.setEditable(true);
+		messageField.setLineWrap(true);
+		messageField.setWrapStyleWord(true);
+		messageField.setFont(Constants.CHAT_FONT);
+
+		messageFieldPane = new JScrollPane(messageField);
+		messageFieldPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		messageFieldPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+		sendButton = new JButton("Send");
+		sendButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) {
+				sendMessage();
+			}
+		});
 		
+		enterKeyListener = new KeyListener() {
+			@Override
+			public void keyPressed(KeyEvent k) {
+				if (k.getKeyCode() == Constants.ENTER_KEY) {
+					sendMessage();
+				}
+			}
+
+			@Override
+			public void keyReleased(KeyEvent k) {}
+
+			@Override
+			public void keyTyped(KeyEvent k) {}
+		};
+		
+		addKeyListener(enterKeyListener);
+		chatFeed.addKeyListener(enterKeyListener);
+		messageField.addKeyListener(enterKeyListener);
+		sendButton.addKeyListener(enterKeyListener);
 		
 		add(chatFeedPane);
+		add(messageFieldPane);
+		add(sendButton);
 		setSize(500, 300);
+		chatFeedPane.setBounds(0, 0, 485, 200);
+		messageFieldPane.setBounds(0, 200, 410, 65);
+		sendButton.setBounds(410, 200, 75, 64);
+
 	}
-	
-	public void sendMessage(String message) {
-		Client.sendMessage(message);
+
+	public void sendMessage() {
+		Client.sendMessage(messageField.getText());
+		System.out.println(messageField.getText());
+		chatFeed.append(messageField.getText() + "\n");
+		messageField.setText("");
 	}
 
 	JMenu getCientMenu() {
 		return clientMenu;
 	}
-	
+
 	JTextArea getChatFeed() {
 		return chatFeed;
 	}
-	
+
 	JScrollPane getChatFeedPane() {
 		return chatFeedPane;
 	}
-	
-	JTextField getMessageField() {
+
+	JTextArea getMessageField() {
 		return messageField;
 	}
-	
+
 	JButton getSendButton() {
 		return sendButton;
 	}
