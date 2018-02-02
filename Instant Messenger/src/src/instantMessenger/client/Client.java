5da69255a93a4faf9a3c8d0f94a3e9f2070d8ae5
Joshua Ciffer <Joshua.Ciffer@motsd.org>
2018-02-01 20:58:45 -0500

    Started networking/I/O streams

--- Instant Messenger/src/src/instantMessenger/client/Client.java
+++ Instant Messenger/src/src/instantMessenger/client/Client.java
@@ -1,11 +1,87 @@
 package src.instantMessenger.client;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import src.instantMessenger.util.Constants;
 
-
+/**
+ * 
+ * @author Joshua Ciffer
+ * @version 02/01/2018
+ */
 public class Client {
 
+	private Socket serverConnection;
+
+	private ObjectInputStream incommingTraffic;
+
+	private ObjectOutputStream outgoingTraffic;
+
+	private String serverIP;
 	
-	public static void sendMessage(String message) {
-		
+	private int serverPort;
+
+	private String userName = "User" + ((int)Math.random() * 10);
+
+	Client() {
+		serverIP = "192.168.1.1";
+		serverPort = 2075;
+		connectToServer();
+
 	}
-	
+
+	void sendMessage(String message) {
+		try {
+			outgoingTraffic.writeObject(userName + " " + Constants.getTime() + " - " + message + "\n");
+			outgoingTraffic.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	void connectToServer() {
+		try {
+			serverConnection = new Socket(InetAddress.getByName(serverIP), serverPort);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		initializeNetworkStreams();
+	}
+
+	private void initializeNetworkStreams() {
+		try {
+			incommingTraffic = new ObjectInputStream(serverConnection.getInputStream());
+			outgoingTraffic = new ObjectOutputStream(serverConnection.getOutputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	String getServerIP() {
+		return serverIP;
+	}
+
+	int getServerPort() {
+		return serverPort;
+	}
+
+	String getUserName() {
+		return userName;
+	}
+
+	void setServerIP(String serverIP) {
+		this.serverIP = serverIP;
+	}
+
+	void setServerPort(int serverPort) {
+		this.serverPort = serverPort;
+	}
+
+	void setUserName(String userName) {
+		this.userName = userName;
+	}
+
 }
