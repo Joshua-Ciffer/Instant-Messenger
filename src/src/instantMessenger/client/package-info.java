001440e56be9b7649569a500167850a1d30339f3
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-02-07 23:46:07 -0500

    Wrote javadoc.

--- src/src/instantMessenger/client/package-info.java
+++ src/src/instantMessenger/client/package-info.java
@@ -1,8 +1,7 @@
 /**
+ * Contains code for the client interface and related networking and back-end code.
  * 
- */
-/**
- * @author Joshua
- *
+ * @author Joshua Ciffer
+ * @version 02/07/2018
  */
 package src.instantMessenger.client;
