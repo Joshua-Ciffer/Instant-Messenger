74929fa794ee0cfe5ad11da646b236a94e5f4c8d
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-04-12 22:56:51 -0400

    Rewrote client model class.

--- src/main/java/src/instantMessenger/client/Client.java
+++ src/main/java/src/instantMessenger/client/Client.java
@@ -1,15 +1,18 @@
 package src.instantMessenger.client;
 
 import java.net.Socket;
-import java.net.Inet4Address;
+import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.io.ObjectInputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.io.IOException;
 
-import src.instantMessenger.util.Constants;
+import static src.instantMessenger.util.Constants.generateUserName;
+import static src.instantMessenger.util.Constants.getTime;
 
 /**
+ * This class represents a single chat client that has a connection to the server.
  * 
  * @author Joshua Ciffer
  * @version 04/12/2018
@@ -24,76 +27,127 @@ public final class Client {
 	/**
 	 * Incoming network traffic.
 	 */
-	private ObjectInputStream inboundTraffic;
+	private BufferedReader inboundTraffic;
 
 	/**
 	 * Outgoing network traffic.
 	 */
 	private PrintWriter outboundTraffic;
 
-	private Inet4Address serverIP;
+	/**
+	 * The server's IP address.
+	 */
+	private InetAddress serverIP;
 
+	/**
+	 * The server's port.
+	 */
 	private short serverPort;
 
+	/**
+	 * The current user's name.
+	 */
 	private String userName;
 
-	Client() throws IOException {
-		userName = "User" + (int)(Math.random() * 1_000);
-		try {
-			serverIP = (Inet4Address)Inet4Address.getByName("192.168.1.1");
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		}
-		serverPort = 123;
-		serverConnection = new Socket(serverIP, serverPort);
-		inboundTraffic = new ObjectInputStream(serverConnection.getInputStream());
-		outboundTraffic = new PrintWriter(serverConnection.getOutputStream());
+	/**
+	 * Constructs a new client.
+	 */
+	Client() {
+		userName = generateUserName();
 	}
 
+	/**
+	 * Sends a text based message over the network through the server connection.
+	 *
+	 * @param message
+	 *        The message to send.
+	 */
 	void sendMessage(String message) {
-		outboundTraffic.println(userName + " " + Constants.getTime() + " - " + message + "\n");
+		outboundTraffic.println(userName + " " + getTime() + " - " + message);
 		outboundTraffic.flush();
 	}
 
+	/**
+	 * Initializes the connection to the server and sets up network streams.
+	 *
+	 * @throws IOException
+	 *         Thrown if a network or IO error occurs.
+	 */
 	void connect() throws IOException {
-
+		serverConnection = new Socket(serverIP, serverPort);
+		inboundTraffic = new BufferedReader(new InputStreamReader(serverConnection.getInputStream()));
+		outboundTraffic = new PrintWriter(serverConnection.getOutputStream());
 	}
 
+	/**
+	 * Terminates the server connection and closes network streams.
+	 */
 	void disconnect() {
 		try {
 			if (serverConnection != null) {
 				serverConnection.close();
 				serverConnection = null;
-			} else if (inboundTraffic != null) {
+			}
+			if (inboundTraffic != null) {
 				inboundTraffic.close();
 				inboundTraffic = null;
-			} else if (outboundTraffic != null) {
+			}
+			if (outboundTraffic != null) {
 				outboundTraffic.close();
 				outboundTraffic = null;
+			}
+			if (serverIP != null) {
+				serverIP = null;
+			}
+			if (serverPort != 0) {
+				serverPort = 0;
 			}
 		} catch (IOException e) {}
 	}
 
+	/**
+	 * @return The server IP.
+	 */
 	String getServerIP() {
 		return serverIP.getHostAddress();
 	}
 
+	/**
+	 * @return The server port.
+	 */
 	short getServerPort() {
 		return serverPort;
 	}
 
+	/**
+	 * @return The user name.
+	 */
 	String getUserName() {
 		return userName;
 	}
 
+	/**
+	 * @param serverIP
+	 *        The server IP to set.
+	 * @throws UnknownHostException
+	 *         Thrown if the IP is invalid.
+	 */
 	void setServerIP(String serverIP) throws UnknownHostException {
-		this.serverIP = (Inet4Address)Inet4Address.getByName(serverIP);
+		this.serverIP = InetAddress.getByName(serverIP);
 	}
 
+	/**
+	 * @param serverPort
+	 *        The server port to set.
+	 */
 	void setServerPort(short serverPort) {
 		this.serverPort = serverPort;
 	}
 
+	/**
+	 * @param userName
+	 *        The user name to set.
+	 */
 	void setUserName(String userName) {
 		this.userName = userName;
 	}
--- src/main/java/src/instantMessenger/util/Constants.java
+++ src/main/java/src/instantMessenger/util/Constants.java
@@ -8,29 +8,36 @@ import java.awt.Font;
  * Values and objects used globally in different parts of the project.
  *
  * @author Joshua Ciffer
- * @version 03/31/2018
+ * @version 04/12/2018
  */
 public interface Constants {
 
 	/**
 	 * Font used in the chat screen.
 	 */
-	public static final Font CHAT_FONT = new Font("Courier new", Font.PLAIN, 12);
+	Font CHAT_FONT = new Font("Courier new", Font.PLAIN, 12);
 
 	/**
 	 * Bold font used in the chat screen.
 	 */
-	public static final Font CHAT_FONT_BOLD = new Font("Courier new", Font.BOLD, 12);
+	Font CHAT_FONT_BOLD = new Font("Courier new", Font.BOLD, 12);
 
 	/**
 	 * ASCII value for the enter key.
 	 */
-	public static final int ENTER_KEY = 13;
+	int ENTER_KEY = 13;
+
+	/**
+	 * @return A randomly generated username.
+	 */
+	static String generateUserName() {
+		return "User" + (int)(Math.random() * 1_000);
+	}
 
 	/**
 	 * @return The current time.
 	 */
-	public static String getTime() {
+	static String getTime() {
 		return DateTimeFormatter.ofPattern("hh:mm a").format(LocalDateTime.now());
 	}
 
