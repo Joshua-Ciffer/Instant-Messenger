f2a76b8f154b3d3b0f452d3012aec12512bc19d8
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-04-21 21:15:43 -0400

    Wrote CancelDialogListener class.

--- src/main/java/src/instantMessenger/client/controller/CancelDialogListener.java
+++ src/main/java/src/instantMessenger/client/controller/CancelDialogListener.java
@@ -6,18 +6,21 @@ import java.awt.event.ActionListener;
 import javax.swing.JDialog;
 
 /**
- * 
+ * Listens for an event created by a cancel button on a dialog box.
  *
- * @author Joshua
+ * @author Joshua Ciffer
  * @version 04/21/2018
  */
-public class CancelListener extends JDialog implements ActionListener {
+public class CancelDialogListener extends JDialog implements ActionListener {
 
 	/**
-	 * 
+	 * Default serial version UID.
 	 */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Closes the current dialog without saving any changes.
+	 */
 	@Override
 	public void actionPerformed(ActionEvent a) {
 		dispose();
