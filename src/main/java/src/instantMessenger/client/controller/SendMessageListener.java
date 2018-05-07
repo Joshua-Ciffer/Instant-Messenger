b65365f6a64643232a9e9badfe5e121f7b7c577a
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-05-07 00:50:43 -0400

    Moved message sending code to separate listener class.

---
+++ src/main/java/src/instantMessenger/client/controller/SendMessageListener.java
@@ -0,0 +1,44 @@
+package src.instantMessenger.client.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import src.instantMessenger.client.view.MessageFieldPanel;
+
+/**
+ * Listens for an event to send a message to the server.
+ *
+ * @author Joshua Ciffer
+ * @version 05/07/2018
+ */
+public class SendMessageListener implements ActionListener {
+
+	/**
+	 * The component that this listener is added to.
+	 */
+	private MessageFieldPanel parentComponent;
+
+	/**
+	 * Constructs a new <code>SendMessageListener</code>.
+	 *
+	 * @param parentComponent
+	 *        The component that this listener is added to.
+	 */
+	public SendMessageListener(MessageFieldPanel parentComponent) {
+		this.parentComponent = parentComponent;
+	}
+
+	/**
+	 * Sends the message entered in the message text field to the server.
+	 */
+	@Override
+	public void actionPerformed(ActionEvent a) {
+		try {
+			parentComponent.getParentFrame().getParentView().getController().sendMessage(parentComponent.getMessageFieldText());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
--- src/main/java/src/instantMessenger/client/view/MessageFieldPanel.java
+++ src/main/java/src/instantMessenger/client/view/MessageFieldPanel.java
@@ -1,21 +1,21 @@
 package src.instantMessenger.client.view;
+
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JScrollPane;
 import javax.swing.JButton;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
 
-import src.instantMessenger.util.Constants;
+import src.instantMessenger.client.controller.SendMessageListener;
+
+import static src.instantMessenger.util.Constants.CHAT_FONT;
 
 /**
- * MessageFieldPanel is a container for the functionality that allows the user to type and send messages.
+ * <code>MessageFieldPanel</code> is a panel that contains functionality that allows the user to type and send messages.
  * 
  * @author Joshua Ciffer
- * @version 02/22/2018
+ * @version 05/07/2018
  */
-final class MessageFieldPanel extends JPanel {
+public final class MessageFieldPanel extends JPanel {
 
 	/**
 	 * Default serial version UID.
@@ -23,47 +23,45 @@ final class MessageFieldPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 
 	/**
+	 * The parent frame that this panel is added to.
+	 */
+	private ClientFrame parentFrame;
+
+	/**
 	 * Allows the user to type their message.
 	 */
 	private JTextArea messageTextField;
-	
+
 	/**
-	 * Allows the message field text field to be scrollable.
+	 * Allows the message text field to be scrollable.
 	 */
 	private JScrollPane messageFieldPane;
-	
+
 	/**
 	 * Sends the message that is entered in the message text field.
 	 */
 	private JButton sendButton;
-	
+
 	/**
-	 * Constructs a new MessageFieldPanel container.
+	 * Constructs a new <code>MessageFieldPanel</code> container.
 	 * 
-	 * @param parentFrame - The parent frame that this container is placed on.
+	 * @param parentFrame
+	 *        The parent frame that this panel is placed on.
 	 */
 	MessageFieldPanel(ClientFrame parentFrame) {
 		super();
+		this.parentFrame = parentFrame;
 		setLayout(null);
 		messageTextField = new JTextArea();
 		messageTextField.setEditable(true);
 		messageTextField.setLineWrap(true);
 		messageTextField.setWrapStyleWord(true);
-		messageTextField.setFont(Constants.CHAT_FONT);
+		messageTextField.setFont(CHAT_FONT);
 		messageFieldPane = new JScrollPane(messageTextField);
 		messageFieldPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 		messageFieldPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		sendButton = new JButton("Send");
-		sendButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent a) {
-				try {
-					parentFrame.getParentView().getController().sendMessage(messageTextField.getText());;
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		sendButton.addActionListener(new SendMessageListener(this));
 		add(messageFieldPane);
 		add(sendButton);
 		setSize(500, 100);
@@ -71,14 +69,19 @@ final class MessageFieldPanel extends JPanel {
 		sendButton.setBounds(410, 0, 75, 49);
 		setVisible(true);
 	}
-	
+
 	/**
-	 * Returns a reference to the message field text pane.
-	 * 
-	 * @return The message field text pane.
+	 * @return The parent frame that this panel is placed on.
 	 */
-	JTextArea getMessageTextField() {
-		return messageTextField;
+	public ClientFrame getParentFrame() {
+		return parentFrame;
 	}
-	
+
+	/**
+	 * @return The message entered in the message text field.
+	 */
+	public String getMessageFieldText() {
+		return messageTextField.getText();
+	}
+
 }
