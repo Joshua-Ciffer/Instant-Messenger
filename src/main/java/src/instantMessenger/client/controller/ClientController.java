86728eae1ae6d07d21ae70802ee79b4f6ae267ad
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-05-06 23:54:32 -0400

    Documented client controller.

--- src/main/java/src/instantMessenger/client/controller/ClientController.java
+++ src/main/java/src/instantMessenger/client/controller/ClientController.java
@@ -7,33 +7,78 @@ import src.instantMessenger.client.model.Client;
 import src.instantMessenger.client.view.ClientView;
 
 /**
- * 
+ * The controller acts as a bridge between the view and model. Essentially, the model servers as the client object and the view displays the information from that object.
+ * Requests from the view to update data in the model pass through the controller, and information to be displayed from the model passes from the controller to the view.
  *
  * @author Joshua Ciffer
- * @version 04/12/2018
+ * @version 05/06/2018
  */
 public class ClientController {
 
 	/**
-	 * 
+	 * The client model associated with this controller and the view.
 	 */
 	private Client model;
 
 	/**
-	 * 
+	 * The view associated with this controller and model.
 	 */
 	private ClientView view;
 
 	/**
-	 *
+	 * Constructs a new <code>ClientController</code>.
 	 *
 	 * @param model
-	 * @param view 
+	 *        The client model object.
+	 * @param view
+	 *        The GUI view object.
 	 */
 	public ClientController(Client model, ClientView view) {
 		this.model = model;
 		this.view = view;
-		this.view.setController(this);
+		this.view.setController(this);	// The view needs a reference to the controller so it can make requests for information from the model.
+	}
+
+	/**
+	 * Connects the client to the server with the IP address and port specified.
+	 *
+	 * @param serverIP
+	 *        The server's IP address.
+	 * @param serverPort
+	 *        The server's port.
+	 * @throws UnknownHostException
+	 *         Thrown if the IP address is invalid.
+	 * @throws IOException
+	 *         Thrown if there was an error connecting to the server.
+	 */
+	public void connect(String serverIP, short serverPort) throws UnknownHostException, IOException {
+		model.connect(serverIP, serverPort);
+	}
+
+	/**
+	 * Disconnects the client from the current server.
+	 */
+	public void disconnect() {
+		model.disconnect();
+	}
+
+	/**
+	 * Sends a message to the server.
+	 *
+	 * @param message
+	 *        The message to be sent.
+	 * @throws IOException
+	 *         Thrown if there was an error writing to the network streams.
+	 */
+	public void sendMessage(String message) throws IOException {
+		model.sendMessage(message);
+	}
+
+	/**
+	 * Terminates the program.
+	 */
+	public void terminate() {
+		disconnect();
 	}
 
 	/**
@@ -45,70 +90,26 @@ public class ClientController {
 	public void changeUserName(String userName) {
 		model.setUserName(userName);
 	}
-	
+
 	/**
-	 *
-	 *
-	 * @param serverIP
-	 * @param serverPort
-	 * @throws UnknownHostException
-	 * @throws IOException
+	 * @return The client's user name.
 	 */
-	public void connect(String serverIP, short serverPort) throws UnknownHostException, IOException {
-		model.connect(serverIP, serverPort);
+	public String getUserName() {
+		return model.getUserName();
 	}
-	
+
 	/**
-	 *
-	 *
-	 * @return
+	 * @return The IP address of the current server.
 	 */
 	public String getServerIP() {
 		return model.getServerIP();
 	}
-	
+
 	/**
-	 *
-	 *
-	 * @return
+	 * @return The port of the current server.
 	 */
 	public short getServerPort() {
 		return model.getServerPort();
 	}
 
-	/**
-	 *
-	 *
-	 * @return
-	 */
-	public String getUserName() {
-		return model.getUserName();
-	}
-	
-	/**
-	 *
-	 *
-	 */
-	public void disconnect() {
-		model.disconnect();
-	}
-
-	/**
-	 *
-	 *
-	 */
-	public void terminate() {
-		disconnect();
-	}
-	
-	/**
-	 *
-	 *
-	 * @param message
-	 * @throws IOException
-	 */
-	public void sendMessage(String message) throws IOException {
-		model.sendMessage(message);
-	}
-	
 }
