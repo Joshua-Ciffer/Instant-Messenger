df05b97c03d522b1c61ad2ddd0684be38288bfcf
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-05-13 22:46:00 -0400

    Did some housekeeping stuff.

--- src/main/java/src/instantMessenger/client/controller/ClientController.java
+++ src/main/java/src/instantMessenger/client/controller/ClientController.java
@@ -9,28 +9,31 @@ import src.instantMessenger.client.view.ClientView;
 
 import static src.instantMessenger.util.Constants.getTime;
 import static src.instantMessenger.util.Constants.MESSAGE_NOT_SENT;
+import static src.instantMessenger.util.Constants.serverDisconnectedMessage;
+import static src.instantMessenger.util.Constants.ALREADY_DISCONNECTED;
 
 /**
- * The controller acts as a bridge between the view and model. Essentially, the model serves as the client object and the view displays the information from that object.
- * Requests from the view to update data in the model pass through the controller, and information to be displayed from the model passes from the controller to the view.
+ * The controller acts as a bridge between the view and model. Essentially, the model serves as the client object and the view displays the information from
+ * that object. Requests from the view to update data in the model pass through the controller, and information to be displayed from the model passes from
+ * the controller to the view.
  *
  * @author Joshua Ciffer
- * @version 05/06/2018
+ * @version 05/13/2018
  */
 public final class ClientController {
 
 	/**
-	 * The client model associated with this controller and the view.
+	 * The client model associated with this controller and view.
 	 */
 	private Client model;
 
 	/**
-	 * The view associated with this controller and the model.
+	 * The client view associated with this controller and model.
 	 */
 	private ClientView view;
 
 	/**
-	 * Handler thread that processes incoming network traffic.
+	 * Handler thread that processes incoming network traffic using the model.
 	 */
 	private NetworkTrafficHandler trafficHandler;
 
@@ -45,16 +48,16 @@ public final class ClientController {
 	public ClientController(Client model, ClientView view) {
 		this.model = model;
 		this.view = view;
-		this.view.setController(this);	// The view needs a reference to the controller so it can make requests for information from the model.
+		this.view.setController(this);	// The view needs a reference to this controller so it can make requests for information from the model.
 		trafficHandler = new NetworkTrafficHandler(this);
 	}
 
 	/**
-	 * Starts running all client threads and prepares program to launch.
+	 * Starts running all client threads and prepares the program to launch.
 	 */
 	public void init() {
 		view.getClientFrame().setVisible(true);
-		trafficHandler.run();
+		trafficHandler.start();
 	}
 
 	/**
@@ -70,6 +73,9 @@ public final class ClientController {
 	 *         Thrown if there was an error connecting to the server.
 	 */
 	public void connect(String serverIP, short serverPort) throws UnknownHostException, IOException {
+		if (model.isConnected()) {
+			disconnect();
+		}
 		model.connect(serverIP, serverPort);
 	}
 
@@ -78,12 +84,22 @@ public final class ClientController {
 	 */
 	public void disconnect() {
 		if (model.isConnected()) {
+			view.appendToChatFeed(serverDisconnectedMessage(getServerIP(), getServerPort()));
 			model.disconnect();
+		} else {
+			view.appendToChatFeed(ALREADY_DISCONNECTED);
 		}
 	}
 
 	/**
-	 * Sends a message to the server.
+	 * @return True if the client is connected to a server, false if not connected.
+	 */
+	public boolean isConnected() {
+		return model.isConnected();
+	}
+
+	/**
+	 * Writes a text based message to the network streams which is then sent to the server.
 	 *
 	 * @param message
 	 *        The message to be sent.
@@ -91,8 +107,8 @@ public final class ClientController {
 	 *         Thrown if there was an error writing to the network streams.
 	 */
 	public void sendMessage(String message) throws IOException {
-		message = getTime() + " - " + getUserName() + ": " + message + "\n";
 		if (model.isConnected()) {
+			message = getTime() + " - " + getUserName() + ": " + message + "\n";
 			model.sendMessage(message);
 			view.appendToChatFeed(message);
 		} else {
@@ -116,16 +132,6 @@ public final class ClientController {
 	}
 
 	/**
-	 * Terminates the program.
-	 */
-	public void terminate() {
-		disconnect();
-		int exitCode = 0;
-		System.out.println("Program terminated with exit code: " + exitCode);
-		System.exit(exitCode);
-	}
-
-	/**
 	 * Appends a message to the chat feed.
 	 *
 	 * @param message
@@ -136,6 +142,18 @@ public final class ClientController {
 	}
 
 	/**
+	 * Terminates the program after disconnected from the current server and stopping all network threads.
+	 */
+	@SuppressWarnings("deprecation")
+	public void terminate() {	// TODO: Figure out how to safely stop threads.
+		disconnect();
+		trafficHandler.stop();
+		int exitCode = 0;
+		System.out.println("Program terminated with exit code: " + exitCode);
+		System.exit(exitCode);
+	}
+
+	/**
 	 * Updates the user name saved in the client model.
 	 * 
 	 * @param userName
@@ -143,13 +161,6 @@ public final class ClientController {
 	 */
 	public void changeUserName(String userName) {
 		model.setUserName(userName);
-	}
-
-	/**
-	 * @return True if the client is connected to a server, false if not connected.
-	 */
-	public boolean isConnected() {
-		return model.isConnected();
 	}
 
 	/**
