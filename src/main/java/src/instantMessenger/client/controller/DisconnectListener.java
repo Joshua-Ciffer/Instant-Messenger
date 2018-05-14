4f97c40b9019fd23bd9657836b47c016eecd1b17
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-05-13 22:44:55 -0400

    Moved disconnect method logic to the controller.

--- src/main/java/src/instantMessenger/client/controller/DisconnectListener.java
+++ src/main/java/src/instantMessenger/client/controller/DisconnectListener.java
@@ -5,14 +5,12 @@ import java.awt.event.ActionListener;
 
 import src.instantMessenger.client.view.ClientFrame;
 
-import static src.instantMessenger.util.Constants.serverDisconnectedMessage;
-import static src.instantMessenger.util.Constants.ALREADY_DISCONNECTED;
-
 /**
- * Listens for an event created by a disconnect button to disconnect the client from the current server.
+ * Listens for an event created by a disconnect button to disconnect the client from the current server. This listener is added to the disconnect button on the
+ * connection sub-menu of the menu bar.
  *
  * @author Joshua Ciffer
- * @version 05/06/2018
+ * @version 05/13/2018
  */
 public final class DisconnectListener implements ActionListener {
 
@@ -36,13 +34,7 @@ public final class DisconnectListener implements ActionListener {
 	 */
 	@Override
 	public void actionPerformed(ActionEvent a) {
-		if (parentFrame.getParentView().getController().isConnected()) {
-			parentFrame.getParentView().getController().appendToChatFeed(
-					serverDisconnectedMessage(parentFrame.getParentView().getController().getServerIP(), parentFrame.getParentView().getController().getServerPort()));
-			parentFrame.getParentView().getController().disconnect();
-		} else {
-			parentFrame.getParentView().getController().appendToChatFeed(ALREADY_DISCONNECTED);
-		}
+		parentFrame.getParentView().getController().disconnect();
 	}
 
 }
